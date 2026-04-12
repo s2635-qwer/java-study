66e3597e13f19f07129a95db5af3b23c56262ce5
s2635-qwer <s2635@e-mirim.hs.kr>
2026-04-13 00:27:58 +0900

    Add files via upload

---
+++ Arith_Test.java
@@ -0,0 +1,15 @@
+public class Arith_Test{
+	public static void main(String[]args){
+		int answer;
+		answer = 3 + 21/5;
+		System.out.println(answer);
+		answer = (3+21) /5;
+		System.out.println(answer);
+		answer = 4 + ((15-7)/4);
+		System.out.println(answer);
+		answer = 13/3+5*2;
+		System.out.println(answer);
+		answer = (int)(5.5%2.0);
+		System.out.println(answer);
+	}
+}
